93b3c592c52c41c1b9e0c87c6654449c48d92bb1
Bracken <BrackenLo@proton.me>
2024-11-01 15:31:44 +0000

    Optional Text

--- feathered/src/lib.rs
+++ feathered/src/lib.rs
@@ -10,18 +10,9 @@ pub use feathered_shipyard as shipyard;
 
 pub use feathered_spatial as spatial;
 
+#[cfg(feature = "text")]
 pub use feathered_text as text;
 
 pub use feathered_tools as tools;
 
 //====================================================================
-
-// pub mod systems {
-//     pub use feathered_render_tools::{
-//         shared::sys_setup_shared_resources,
-//         sys_finish_main_render_pass, sys_setup_encoder, sys_setup_render_pass,
-//         sys_setup_renderer_components, sys_submit_encoder,
-//         texture::{sys_resize_depth_texture, sys_setup_depth_texture},
-//     };
-// }
-// ====================================================================
